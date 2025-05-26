9219f324c306899c6f9aecafbfc580df25d1e59e
vxdnt <vedantkonde36@gmail.com>
2025-05-26 20:32:38 +0530

    Milestone-VPDB-Relationship

--- models/database.py
+++ models/database.py
@@ -12,7 +12,7 @@ class User(db.Model):
     city = db.Column(db.String(32), nullable=False)
     is_admin = db.Column(db.Boolean, nullable = False, default = False)
 
-    vehicles = db.relationship('Vehicle', backref='user', lazy=True)
+    car = db.relationship('Car', backref='user', lazy=True)
     bookings = db.relationship('Bookings', backref='user', lazy=True)
 
     @property
@@ -26,16 +26,16 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class Vehicle(db.Model):
-    __tablename__ = 'vehicle'
+class Car(db.Model):
+    __tablename__ = 'car'
     id = db.Column(db.Integer, primary_key=True)
-    vehicle_number = db.Column(db.String(32), unique=True, nullable=False)
+    plate_number = db.Column(db.String(32), unique=True, nullable=False)
     model = db.Column(db.String(512), nullable=False)
     name = db.Column(db.String(32), nullable=False)
     color = db.Column(db.String(32), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    bookings = db.relationship('Bookings', backref='vehicle', lazy=True)
+    bookings = db.relationship('Bookings', backref='car', lazy=True)
 
 class Spots(db.Model):
     __tablename__ = 'spot'
@@ -52,7 +52,7 @@ class Bookings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     spot_id = db.Column(db.Integer, db.ForeignKey('spot.id'), nullable=False)
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
     booking_date = db.Column(db.Date, nullable=False)
     booking_time = db.Column(db.Time, nullable=False)
     is_active = db.Column(db.Boolean, nullable = False, default = True)
